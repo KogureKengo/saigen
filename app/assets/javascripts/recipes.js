2d40e1b6a1727b837ff29c1705b63f41d1c0111a
KogureKengo <a.kagi.ten@gmail.com>
2016-01-11 20:21:37 +0900

    投稿ページ完成

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -14,20 +14,22 @@ function readURL(input) {
 
 function readURL2(input) {
   if (input.files && input.files[0]) {
-    var id = input.id;
-    var img = 'img_prev2';
-    console.log(id);
+    var id = input.id.replace( /recipe_directions_attributes_/g , "" );
+    id = id.replace( /_image/g , "" );
+    id = 1 + parseInt(id);
     var reader = new FileReader();
 
     reader.onload = function (e) {
-      $('#' + img)
+      $('#element'+ id)
         .attr('src', e.target.result);
         // .width(150)
         // .height(200);
+        console.log(id);
     };
     reader.readAsDataURL(input.files[0]);
   }
 }
+
 
 var lastNumber = 1;
 
@@ -38,6 +40,11 @@ $(document).on('fields_added.nested_form_fields', function(event, params) {
     lastNumber++;
   }
 
+  $(function(){
+      $('.ingredients table td img').each(function(i){
+          $(this).attr('id','element' + (i+1));
+      });
+  });
   if (event.type === "fields_added") {
     changeValOfLastRecipe();
   }
